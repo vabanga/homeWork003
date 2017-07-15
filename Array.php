cf7e3c16b0fcdfd54b42ff32e107430bb0531f53
vabanga <gohavorsin@gmail.com>
2017-07-15 12:32:19 +1000

    Beginning of work

---
+++ Array.php
@@ -0,0 +1,15 @@
+<?php
+
+$array = array(
+    "Eurasia" => array (
+        "1" => "Himalayan bear",
+        "2" => ""
+    ),
+    "Africa" => "",
+    "North America" => "",
+    "South Americ" => "",
+    "Australia" => "",
+    "Antarctica" => ""
+);
+
+?>

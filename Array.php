744bb437300cbe87e725d18d6dbcf89594e9c106
vabanga <gohavorsin@gmail.com>
2017-07-15 12:39:58 +1000

    change array

--- Array.php
+++ Array.php
@@ -3,11 +3,20 @@
 $array = array(
     "Eurasia" => array (
         "1" => "Himalayan bear",
-        "2" => ""
+        "2" => "Red Wolf"
     ),
-    "Africa" => "",
-    "North America" => "",
-    "South Americ" => "",
+    "Africa" => array (
+        "3" => "ZEBRAS",
+        "4" => "GIRAFFE"
+    ),
+    "North America" => array (
+        "5" => "Coyote",
+        "6" => "Puma"
+    ),
+    "South Americ" => array (
+        "7" => "Lama",
+        "8" => "Jaguar"
+    ),
     "Australia" => "",
     "Antarctica" => ""
 );
